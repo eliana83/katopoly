340447649bef765543b8a52984ea18d493890bf5
eliana83 <48333225+eliana83@users.noreply.github.com>
2019-03-13 22:14:16 +0100

    Add files via upload

--- tests/game.tests.js
+++ tests/game.tests.js
@@ -53,16 +53,20 @@ describe('Game Tests', () => {
     it('should return round = 20', () => {
       const players = [{
         name: 'Horse',
-        position: 0,
+        prevPosition: 0,
+        currentPosition: 0,
+        balance: 0,
         round: 0
        },
        {
          name: 'Car',
-         position: 0,
+         prevPosition: 0,
+         currentPosition: 0,
+         balance: 0,
          round: 0
        } ];
-       const maxRound = 20;
-       const maxLocation = 40;
+      const maxRound = 20;
+      const maxLocation = 40;
       const result = game.playGame(players, maxRound, maxLocation);
       expect(result).to.have.property('roundNumber', maxRound);
     });
@@ -70,12 +74,16 @@ describe('Game Tests', () => {
     it('Both players should play 20 rounds', () => {
       const players = [{
         name: 'Horse',
-        position: 0,
+        prevPosition: 0,
+        currentPosition: 0,
+        balance: 0,
         round: 0
        },
        {
          name: 'Car',
-         position: 0,
+         prevPosition: 0,
+         currentPosition: 0,
+         balance: 0,
          round: 0
        } ];
        const maxRound = 20;
@@ -85,7 +93,166 @@ describe('Game Tests', () => {
       expect(players[1].round).to.equal(maxRound);
     });
     
-    
+    // Go check
+    it('when Player lands on Go, their balance should increase by $200', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 0,
+        currentPosition: 0,
+        balance: 50,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 250);
+    });
 
+    it('when Player lands on a position different from go, their balance should not change', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 1,
+        currentPosition: 7,
+        balance: 50,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 50);
+    });
+    
+    it('when Player passes over on Go, their balance should increase by $200', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 38,
+        currentPosition: 3,
+        balance: 50,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 250);
+    });
+
+    it('when Player starts on go and does not land or pass over go, their balance should not change', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 0,
+        currentPosition: 7,
+        balance: 50,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 50);
+    });
+
+    // Go to jail check
+    it('when Player starts before Go To Jail, lands on Go To Jail, ends up on Just Visiting, their balance should not change', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 25,
+        currentPosition: 30,
+        balance: 50,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 50);
+      expect(player).to.have.property('currentPosition', 10);
+    });
+
+    it('when Player starts before Go To Jail and passes over Go To Jail, their balance should not change and they should land on correct position', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 25,
+        currentPosition: 32,
+        balance: 50,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 50);
+      expect(player).to.have.property('currentPosition', 32);
+    });
+
+
+    // Income Tax check
+    it('when Player with balance $1800 lands on Income Tax, their balance should decrease by $180 ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 2,
+        currentPosition: 4,
+        balance: 1800,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 1800 -180);
+    });
+
+    it('when Player with balance $2200 lands on Income Tax, their balance should decrease by $200 ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 2,
+        currentPosition: 4,
+        balance: 2200,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 2000);
+    });
+
+    it('when Player with balance $0 lands on Income Tax, their balance should not decrease ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 2,
+        currentPosition: 4,
+        balance: 0,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 0);
+    });
+
+    it('when Player with balance $2000 lands on Income Tax, their balance should decrease by $200 ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 2,
+        currentPosition: 4,
+        balance: 2000,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 1800);
+    });
+
+    it('when Player passes over Income Tax, their balance should not decrease ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 2,
+        currentPosition: 5,
+        balance: 700,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 700);
+    });
+
+    // Luxury Tax check
+    it('when Player lands on Luxury Tax, their balance should decrease by $75 ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 32,
+        currentPosition: 38,
+        balance: 2000,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 2000 -75);
+    });
+    it('when Player passes Luxury Tax, their balance should not change ', () => {
+      const player = {
+        name: 'Horse',
+        prevPosition: 32,
+        currentPosition: 39,
+        balance: 2000,
+        round: 0
+       }
+      game.checkLandingPosition(player);
+      expect(player).to.have.property('balance', 2000);
+    });
 });
 
--- tests/player.tests.js
+++ tests/player.tests.js
@@ -3,19 +3,31 @@ const player = require('../src/player');
 
 describe('Player Tests', () => {
   it('should return position 7 when player on position 0 rolls a total of 7', () => {
-    const playerPosition = 0;
+    const playerObj = {
+      //name = 'One',
+      prevPosition : 0,
+      currentPosition : 0,
+      balance: 0,
+      round : 0
+    };
     const diceScore = 7;
     const maxLocation = 40;
-    const result = player.getNewPosition(playerPosition, diceScore, maxLocation);
-    expect(result).to.equal(7);
+    player.getNewPosition(playerObj, diceScore, maxLocation);
+    expect(playerObj.currentPosition).to.equal(7);
     });
 
   it('should return position 5 when player on position 39 rolls a total of 6', () => {
-    const playerPosition = 39;
+    const playerObj = {
+      //name = 'One',
+      prevPosition : 36,
+      currentPosition : 39,
+      balance: 0,
+      round : 1
+    };
     const diceScore = 6
     const maxLocation = 40;
-    const result = player.getNewPosition(playerPosition, diceScore, maxLocation);
-    expect(result).to.equal(5);
+    player.getNewPosition(playerObj, diceScore, maxLocation);
+    expect(playerObj.currentPosition).to.equal(5);
    });
 
    it('should return an array of objects of the same length as the input', () => {
@@ -24,13 +36,22 @@ describe('Player Tests', () => {
     expect(result.length).to.equal(playerNames.length);
   });
 
-  it('should return an array of objects with property name, position and round', () => {
+  it('should return an array of objects with property name, prevPosition, currentPosition, position and round', () => {
     const playerNames = ['Horse', 'Car'];
     const result = player.initPlayers(playerNames);
     for (let i = 0; i < playerNames.length; i++) {
       expect(result[i]).to.have.property('name', playerNames[i]);
-      expect(result[i]).to.have.property('position', 0);
+      expect(result[i]).to.have.property('prevPosition');
+      expect(result[i]).to.have.property('currentPosition', 0);
       expect(result[i]).to.have.property('round', 0);
+    }
+  });
+
+  it('should return an array of objects with property balance', () => {
+    const playerNames = ['Horse', 'Car'];
+    const result = player.initPlayers(playerNames);
+    for (let i = 0; i < playerNames.length; i++) {
+      expect(result[i]).to.have.property('balance', 0);
     }
   });
 });
